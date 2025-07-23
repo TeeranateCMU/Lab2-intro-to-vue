09e00782e92c8c512e41d8633df9c737cd541f73
teeranate <teeranate_tho@cmu.ac.th>
2025-07-23 22:40:08 +0700

    8.7 add eventcarddetail

--- Lab2-intro-to-vue/src/views/HomeView.vue
+++ Lab2-intro-to-vue/src/views/HomeView.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import EventCard from '@/components/EventCard.vue'
 import type { Event } from '@/types'
-import { ref } from 'vue'
+import EventDetailsCard from '@/components/EventCardDetail.vue'
+import { ref } from 'vue' 
 const events = ref<Event[]>([
-
   {
-   id: 5928101,
-   category: 'animal welfare',
+    id: 5928101,
+    category: 'animal welfare',
     title: 'Cat Adoption Day',
     description: 'Find your new feline friend at this event.',
     location: 'Meow Town',
@@ -15,24 +15,23 @@ const events = ref<Event[]>([
     petsAllowed: true,
     organizer: 'Kat Laydee'
   },
-
   {
-  id: 4582797,
-   category: 'food',
-   title: 'Community Gardening',
+    id: 4582797,
+    category: 'food',
+    title: 'Community Gardening',
     description: 'Join us as we tend to the community edible plants.',
     location: 'Flora City',
     date: 'March 14, 2022',
-   time: '10:00',
+    time: '10:00',
     petsAllowed: true,
     organizer: 'Fern Pollin'
   },
-  {   
-     id: 8419988,
+  {
+    id: 8419988,
     category: 'sustainability',
     title: 'Beach Cleanup',
-   description: 'Help pick up trash along the shore.',
-   location: 'Playa Del Carmen',
+    description: 'Help pick up trash along the shore.',
+    location: 'Playa Del Carmen',
     date: 'July 22, 2022',
     time: '11:00',
     petsAllowed: false,
@@ -43,15 +42,16 @@ const events = ref<Event[]>([
 
 <template>
   <div class="events">
-    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <div v-for="event in events" :key="event.id">
+      <EventCard :event="event" />
+      <EventDetailsCard :event="event" /> </div>
   </div>
 </template>
-<style scoped>
 
+<style scoped>
 .events {
   display: flex;
   flex-direction: column;
   align-items: center;
 }
 </style>
-
